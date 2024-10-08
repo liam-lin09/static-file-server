3842b08ca6dbd5b2604abe6f16eb5784f8d2f861
LIAM.L <liam.lin@sjfood.com>
2024-10-08 03:52:28 -0700

    update

--- build.sh
+++ build.sh
@@ -6,5 +6,14 @@ cupArch="$(uname -m)"
 packageName="static-file-server"
 packageFile=$packageName"_"$operateSystem"_"$cupArch".tar.gz"
 
+echo "开始构建 $packageName"
 go build -o dist/$packageName
+
+echo "开始打包 $packageFile"
 tar -zcvf dist/$packageFile dist/$packageName
+
+if [ -f dist/$packageFile ]; then
+    echo "打包成功，文件路径为：dist/$packageFile"
+else
+    echo "打包失败"
+fi
