cb2ab62451c3e2ab2ce5334f3ff166a80e8eb8b5
LIAM.L <liam.lin@sjfood.com>
2024-10-08 04:40:16 -0700

    update

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
-# 操作系统名称
-operateSystem="$(uname -s | tr '[:upper:]' '[:lower:]')"
-# cpu架构
-cupArch="$(uname -m)"
 # 应用名
 packageName="static-file-server"
-packageFile=$packageName"_"$operateSystem"_"$cupArch".tar.gz"
+packageFile=$packageName"_"$(go env GOHOSTOS)"_"$(go env GOHOSTARCH)".tar.gz"
 
 echo "开始构建 $packageName"
 go build -o dist/$packageName
 
 echo "开始打包 $packageFile"
-tar -zcvf dist/$packageFile dist/$packageName
+cd dist
+tar -zcvf $packageFile $packageName
 
-if [ -f dist/$packageFile ]; then
+if [ -f $packageFile ]; then
     echo "打包成功，文件路径为：dist/$packageFile"
 else
     echo "打包失败"
