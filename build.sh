85770f4f61fd0e28ae518980c522f87032967988
LIAM.L <liam.lin@sjfood.com>
2024-10-08 17:46:49 -0700

    update

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 # 应用名
-packageName="static-file-server"
-packageFile=$packageName"_"$(go env GOHOSTOS)"_"$(go env GOHOSTARCH)".tar.gz"
+appName="static-file-server"
+appFile="$appName$(go env GOEXE)"
+packageFile=$appName"_"$(go env GOHOSTOS)"_"$(go env GOHOSTARCH)".tar.gz"
 
-echo "开始构建 $packageName"
-go build -o dist/$packageName
+echo "开始构建 $appFile"
+go build -o dist/$appFile
 
 echo "开始打包 $packageFile"
 cd dist
-tar -zcvf $packageFile $packageName
+tar -zcvf $packageFile $appFile
 
 if [ -f $packageFile ]; then
     echo "打包成功，文件路径为：dist/$packageFile"
